93c36a3a6e7c094942c9246246369f22fc452138
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-02 13:22:28 -0400

    cleaned up main handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,22 +4,23 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // const postData = await Post.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
 
     
-    const posts = postData.map((post) => this.post.get({ plain: true }));
+    // const posts = postData.map((post) => this.post.get({ plain: true }));
 
 
     res.render('homepage', { 
-      posts, 
-      logged_in: req.session.logged_in 
+      // layout: "homepage",
+      // posts, 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
