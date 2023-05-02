18c168b4b3abdd86cfad64f53d971555de229ddd
dannydelts94 <126992132+dannydelts94@users.noreply.github.com>
2023-05-02 13:11:22 -0400

    post js

---
+++ public/js/post.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const city = document.querySelector('#post-city').value.trim();
+    const state = document.querySelector('#post-state').value.trim()
+    const description = document.querySelector('#post-desc').value.trim();
+  
+    if (title && city && state && description) {
+      const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, city, state, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create post');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.post-list')
+    .addEventListener('click', delButtonHandler);
