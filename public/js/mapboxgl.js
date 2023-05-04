2763bc17ffad7cb4926f8ab13e9cb0cbec334b57
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-04 11:42:35 -0400

    map moved to front end

--- public/js/mapboxgl.js
+++ public/js/mapboxgl.js
@@ -1,17 +1,10 @@
-const Post = require('../models');
-var geo = require('mapbox-geocoding')
-geo.setAccessToken('pk.eyJ1IjoidGdvbGQxIiwiYSI6ImNsaDdzejVzNjAxdGYzam13MGkzOXpmdmsifQ.mTffg5HogZ3NwE5ibJzpIg');
-
-geo.geocode('mapbox.places', `${post.city}, ${post.state}`, function (err, geoData) {
-    console.log(geoData);
-});
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGdvbGQxIiwiYSI6ImNsaDdzejVzNjAxdGYzam13MGkzOXpmdmsifQ.mTffg5HogZ3NwE5ibJzpIg';
 const map = new mapboxgl.Map({
     container: 'map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [39.645891, -99.311362],
+    center: [-99, 32],
     zoom: 11.15
 });
 
@@ -33,7 +26,7 @@ map.on('load', () => {
                     },
                     'geometry': {
                         'type': 'Point',
-                        'coordinates': [32.080776, -81.092164]
+                        'coordinates': [-81, 32]
                     }
                 },
             
@@ -53,31 +46,31 @@ map.on('load', () => {
 
     // When a click event occurs on a feature in the places layer, open a popup at the
     // location of the feature, with description HTML from its properties.
-    map.on('click', 'places', (e) => {
-        // Copy coordinates array.
-        const coordinates = e.features[0].geometry.coordinates.slice();
-        const description = e.features[0].properties.description;
+    // map.on('click', 'places', (e) => {
+    //     // Copy coordinates array.
+    //     const coordinates = e.features[0].geometry.coordinates.slice();
+    //     const description = e.features[0].properties.description;
 
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
+    //     // Ensure that if the map is zoomed out such that multiple
+    //     // copies of the feature are visible, the popup appears
+    //     // over the copy being pointed to.
+    //     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    //         coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    //     }
 
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(map);
-    });
+    //     new mapboxgl.Popup()
+    //         .setLngLat(coordinates)
+    //         .setHTML(description)
+    //         .addTo(map);
+    // });
 
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'places', () => {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+    // // Change the cursor to a pointer when the mouse is over the places layer.
+    // map.on('mouseenter', 'places', () => {
+    //     map.getCanvas().style.cursor = 'pointer';
+    // });
 
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'places', () => {
-        map.getCanvas().style.cursor = '';
-    });
+    // // Change it back to a pointer when it leaves.
+    // map.on('mouseleave', 'places', () => {
+    //     map.getCanvas().style.cursor = '';
+    // });
 });
